e4fde14b5a479ab7e8fd6dc82ffbdb4913c28756
James M. Mazur <jamesmazur6@gmail.com>
2015-04-01 13:14:58 -0400

    Make compilation succeed (after a small issue with the merge).

--- src/algo/Solver.cpp
+++ src/algo/Solver.cpp
@@ -4,7 +4,6 @@
 
 #include "continuous/demo/Continue.h"
 #include "discrete/floodfill/FloodFill.h"
-#include "discrete/mack/MackAlgo.h"
 #include "discrete/misc/Forward.h"
 #include "discrete/wallFollow/LeftWallFollow.h"
 #include "discrete/wallFollow/RightWallFollow.h"
@@ -26,7 +25,6 @@ void Solver::solve() {
     //demo::Continue algo;
     danAlgo algo;
     //floodfill::FloodFill algo;
-    //mack::MackAlgo algo;
     //misc::Forward algo;
     //wallFollow::RightWallFollow algo;
     //wallFollow::LeftWallFollow algo;
